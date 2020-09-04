f5bf7496cd758e15400215c62ba0c199c2b1bf22
Grimless <kyle@kyleroucis.com>
2020-09-04 14:28:08 -0400

    Fix default prod host config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :the_great_machine, TheGreatMachineWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  http: [port: 80],
+  url: [host: "example.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
